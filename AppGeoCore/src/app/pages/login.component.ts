f87f88b116522c9fdcc97e8e3c21e3f381219dd9
eve <jami.159261@e.unavarra.es>
2025-09-09 12:30:25 +0200

    Añadir componentes de encabezado y pie de página, implementar enrutamiento y mejorar la estructura de la aplicación

--- AppGeoCore/src/app/pages/login.component.ts
+++ AppGeoCore/src/app/pages/login.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
